d25c50f076a194c3706608cc959788528810089a
Lukasz Bekus <beckon@interia.pl>
2019-10-23 10:33:30 +0200

    szablon

--- HRall.cpp
+++ HRall.cpp
@@ -1,9 +1,31 @@
 ﻿#include <iostream>
+#include<vector>
 
 using namespace std;
 
-int main()
-{
-	return 0;
+long arrayManipulation(int n, vector<vector<int>> queries) {
+
+
 }
 
+int main()
+{
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> queries(m);
+	for (int i = 0; i < m; i++) {
+		queries[i].resize(3);
+
+		for (int j = 0; j < 3; j++) {
+			cin >> queries[i][j];
+		}
+	}
+
+	long result = arrayManipulation(n, queries);
+
+	cout << result << "\n";
+
+	return 0;
+}

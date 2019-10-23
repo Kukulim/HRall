b5d7e8513fdd5cee6bc915f639048b6e27c6b0e8
Lukasz Bekus <beckon@interia.pl>
2019-10-23 16:54:35 +0200

    szablon

--- HRall.cpp
+++ HRall.cpp
@@ -1,1 +1,67 @@
-﻿
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+string ltrim(const string&);
+string rtrim(const string&);
+vector<string> split(const string&);
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+vector<int> dynamicArray(int n, vector<vector<int>> queries) {
+
+}
+
+int main()
+{
+
+
+	string first_multiple_input_temp;
+	getline(cin, first_multiple_input_temp);
+
+	vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+	int n = stoi(first_multiple_input[0]);
+
+	int q = stoi(first_multiple_input[1]);
+
+	vector<vector<int>> queries(q);
+
+	for (int i = 0; i < q; i++) {
+		queries[i].resize(3);
+
+		string queries_row_temp_temp;
+		getline(cin, queries_row_temp_temp);
+
+		vector<string> queries_row_temp = split(rtrim(queries_row_temp_temp));
+
+		for (int j = 0; j < 3; j++) {
+			int queries_row_item = stoi(queries_row_temp[j]);
+
+			queries[i][j] = queries_row_item;
+		}
+	}
+
+	vector<int> result = dynamicArray(n, queries);
+
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i];
+
+		if (i != result.size() - 1) {
+			cout << "\n";
+		}
+	}
+
+	cout << "\n";
+	return 0;
+}

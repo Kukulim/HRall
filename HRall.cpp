a013c1200af675a2e4bad038a83502d97811b2ae
Lukasz Bekus <beckon@interia.pl>
2019-10-24 12:43:39 +0200

    gut

--- HRall.cpp
+++ HRall.cpp
@@ -2,23 +2,14 @@
 using namespace std;
 
 int pageCount(int ilosc_stron, int szukana) {
-	int* ksiazka = new int[ilosc_stron];
-	int stron_od_poczatku = 0;
-	int stron_od_konca = ilosc_stron;
-	for (int i = 0; i < ilosc_stron; i++)
-		ksiazka[i] = i;
-	for (int i = 0; i < ilosc_stron; i++)
-	{
-		stron_od_poczatku++;
-		if (i == szukana)
-			break;
-	}
-	stron_od_konca = ilosc_stron - szukana;
-	cout << stron_od_poczatku << "od poczatku"<<endl;
-	cout << stron_od_konca << "od konca"<< endl;
-	if (stron_od_konca / 2 < stron_od_poczatku / 2)
-		return stron_od_konca;
-	return stron_od_poczatku/2;
+	int totalfront = ilosc_stron/2;
+	int targetfront = szukana/2;
+	int targetback = totalfront - targetfront;
+	
+	
+	if (targetfront < targetback)
+		return targetfront;
+	return targetback;
 }
 
 int main()

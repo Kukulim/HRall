512aa686151123276b089c0cdd0ade5b3781d43b
Lukasz Bekus <beckon@interia.pl>
2019-10-24 13:53:36 +0200

    gut

--- HRall.cpp
+++ HRall.cpp
@@ -3,28 +3,34 @@ using namespace std;
 
 int main()
 {
-	int dlugosc_wycieczki;
-	cin >> dlugosc_wycieczki;
-	int poziom = 0;
-	int doliny = 0;
-	char* wycieczka = new char[dlugosc_wycieczki];
-	for (int i = 0; i < dlugosc_wycieczki; i++)
+	int gotowka,ilosc_klawiatur,ilosc_usb;
+	cin >> gotowka >> ilosc_klawiatur >> ilosc_usb;
+	int* klawiatura = new int[ilosc_klawiatur];
+	int* usb = new int[ilosc_usb];
+	for (int i = 0; i < ilosc_klawiatur; i++)
 	{
-		cin >> wycieczka[i];
+		cin >> klawiatura[i];
 	}
-	for (int i = 0; i < dlugosc_wycieczki; i++)
-		{
-			if (wycieczka[i] == 'U')
-			{
-				poziom++;
-				if (poziom == 0)
-					doliny++;
-			}
-			else if (wycieczka[i] == 'D')
-				poziom--;
-		}
+	for (int i = 0; i < ilosc_usb; i++)
+	{
+		cin >> usb[i];
+	}
 
-		cout << doliny;
+	int suma=0;
+	int max_suma=0;
+	for (int j = 0; j < ilosc_klawiatur; j++)
+	{
+		for (int i = 0; i < ilosc_usb; i++)
+		{
+			suma = klawiatura[j] + usb[i];
+			if ((suma > max_suma) && suma <= gotowka)
+				max_suma = suma;
+		}
+		suma = 0;
+	}
+	if (max_suma == 0)
+		max_suma = -1;
+	cout << max_suma;
 
 	return 0;
 }

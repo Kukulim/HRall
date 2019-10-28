3667b0f707df474a6b2af404e9407b412e4a8750
Lukasz Bekus <beckon@interia.pl>
2019-10-28 09:17:31 +0100

    test

--- HRall.cpp
+++ HRall.cpp
@@ -1,9 +1,25 @@
-﻿#include <iostream>
-
+﻿#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
 using namespace std;
 
-int main()
-{
+
+int main() {
+	int lini_kodu,prob;
+	cin >> lini_kodu,prob;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	string* tabela = new string[lini_kodu];
+	for (int i = 0; i <lini_kodu; i++)
+	{
+		getline(cin, tabela[i]);
+	}
+
+
+
+
 	return 0;
 }
-

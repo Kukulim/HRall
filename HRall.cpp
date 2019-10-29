533bb8a06d3773763d9fc7abd072460375d064db
Lukasz Bekus <beckon@interia.pl>
2019-10-29 12:51:50 +0100

    gut

--- HRall.cpp
+++ HRall.cpp
@@ -1,54 +1,51 @@
 ﻿#include <iostream>
-#include <string>
-#include <sstream>
 #include <exception>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-/*class BadLengthException {
+class Server {
 private:
-	int n;
+	static int load;
 public:
-	BadLengthException(int errornumber) {
-		n = errornumber;
-	}
-
-	int what() {
-		return n;
-	}
-};*/
-
-
-bool checkUsername(string username) {
-	bool isValid = true;
-	int n = username.length();
-	if (n < 5) {
-		throw n;
-	}
-	for (int i = 0; i < n - 1; i++) {
-		if (username[i] == 'w' && username[i + 1] == 'w') {
-			isValid = false;
+	static int compute(long long A, long long B) {
+		load += 1;
+		if (A < 0) {
+			throw std::invalid_argument("A is negative");
 		}
+		vector<int> v(A, 0);
+		int real = -1, cmplx = sqrt(-1);
+		if (B == 0) throw 0;
+		real = (A / B) * real;
+		int ans = v.at(B);
+		return real + A - B * ans;
 	}
-	return isValid;
-}
+	static int getLoad() {
+		return load;
+	}
+};
+int Server::load = 0;
 
 int main() {
 	int T; cin >> T;
 	while (T--) {
-		string username;
-		cin >> username;
+		long long A, B;
+		cin >> A >> B;
 		try {
-			bool isValid = checkUsername(username);
-			if (isValid) {
-				cout << "Valid" << '\n';
-			}
-			else {
-				cout << "Invalid" << '\n';
-			}
+			cout <<Server::compute(A, B)<<endl;
 		}
-		catch (int e) {
-			cout << "Too short: " << e << '\n';
+		catch (bad_alloc & error) {
+			cout << "Not enough memory" << endl;
 		}
+		catch (exception & error) {
+			cout << "Exception: " << error.what() << endl;
+		}
+		catch (...) {
+			cout << "Other Exception" << endl;
+		}
+		cout << Server::getLoad() << endl;
+		return 0;
 	}
-	return 0;
 }

ee74e40fa1f31fe27756828ef826ff278f182dbe
Lukasz Bekus <beckon@interia.pl>
2019-10-29 13:52:34 +0100

    gut

--- HRall.cpp
+++ HRall.cpp
@@ -1,51 +1,113 @@
-﻿#include <iostream>
-#include <exception>
-#include <string>
-#include <stdexcept>
+﻿#include <cmath>
+#include <cstdio>
 #include <vector>
-#include <cmath>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-
-class Server {
-private:
-	static int load;
+int sutdent_id = 0;
+int professon_id = 0;
+class Person
+{
 public:
-	static int compute(long long A, long long B) {
-		load += 1;
-		if (A < 0) {
-			throw std::invalid_argument("A is negative");
-		}
-		vector<int> v(A, 0);
-		int real = -1, cmplx = sqrt(-1);
-		if (B == 0) throw 0;
-		real = (A / B) * real;
-		int ans = v.at(B);
-		return real + A - B * ans;
-	}
-	static int getLoad() {
-		return load;
-	}
+	virtual void putdata() = 0;
+	virtual void getdata() = 0;
+private:
 };
-int Server::load = 0;
+class Professor :public Person
+{
+public:
+	Professor();
+	~Professor();
+	virtual void getdata()
+	{
+		cin >> name >> age >> publikations;
+	}
+	virtual void putdata()
+	{
+		cout << name << " " << age << " " << publikations << " " << id<<endl;
+	}
+private:
+	string name;
+	int age;
+	int publikations;
+	int id;
+};
+
+Professor::Professor()
+{
+	id=++professon_id;
+}
+
+Professor::~Professor()
+{
+}
+class Student :public Person
+{
+public:
+	Student();
+	~Student();
+	virtual void getdata()
+	{
+		cin >> name >> age;
+		for (int i = 0; i < 6; i++)
+		{
+			cin >> marks[i];
+		}
+	}
+	virtual void putdata()
+	{
+		cout << name << " " << age << " " << oblicz_sume_marks() << " " << id<<endl;
+	}
+	int oblicz_sume_marks()
+	{
+		int suma = 0;
+		for (int i = 0; i < 6; i++)
+		{
+			suma+= marks[i];
+		}
+		return suma;
+
+	}
+private:
+	string name;
+	int age;
+	int marks[6];
+	int id;
+};
+
+Student::Student()
+{
+	id = ++sutdent_id;
+}
+
+Student::~Student()
+{
+}
 
 int main() {
-	int T; cin >> T;
-	while (T--) {
-		long long A, B;
-		cin >> A >> B;
-		try {
-			cout <<Server::compute(A, B)<<endl;
+
+	int n, val;
+	cin >> n;//The number of objects that is going to be created.
+	const int g = 4;
+	Person* per[g];
+
+	for (int i = 0; i < n; i++) {
+
+		cin >> val;
+		if (val == 1) {
+			// If val is 1 current object is of type Professor
+			per[i] = new Professor;
+
 		}
-		catch (bad_alloc & error) {
-			cout << "Not enough memory" << endl;
-		}
-		catch (exception & error) {
-			cout << "Exception: " << error.what() << endl;
-		}
-		catch (...) {
-			cout << "Other Exception" << endl;
-		}
-		cout << Server::getLoad() << endl;
-		return 0;
+		else per[i] = new Student; // Else the current object is of type Student
+
+		per[i]->getdata(); // Get the data from the user.
+
 	}
+
+	for (int i = 0; i < n; i++)
+		per[i]->putdata(); // Print the required output for each object.
+
+	return 0;
+
 }
